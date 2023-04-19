9bf6d6720ad495e95673c101dd801d18aba071fb
Julio Rugoloe <juliorugolo@gmail.com>
2023-04-19 15:51:32 -0300

    implementando estrutura do projeto

---
+++ src/redux/reducers/index.js
@@ -0,0 +1,12 @@
+import { combineReducers } from 'redux';
+import game from './game';
+import login from './login';
+
+const rootReducer = combineReducers(
+  {
+    login,
+    game,
+  },
+);
+
+export default rootReducer;
